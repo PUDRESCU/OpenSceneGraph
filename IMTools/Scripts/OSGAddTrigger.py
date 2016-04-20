c672420dc111c35f7ebdefaef1989af918fd2764
Jie Xu <jie.xu@image-metrics.com>
2016-04-20 15:19:57 -0700

    * it will show a warning message when user set hide/show trigger action to global 3D animation

--- IMTools/Scripts/OSGAddTrigger.py
+++ IMTools/Scripts/OSGAddTrigger.py
@@ -1,27 +1,31 @@
 import maya.mel;
 import maya.cmds;
+import functools;
 
 TRIGGERS_DEF = 'none:mouthopen:mouthclose';
 ACTION_DEF = 'run:pause:hide:show';
 
-def updateUI(name_prefix):
-    is_apply_to_3danimation = maya.cmds.getAttr(name_prefix+'.applyToGlobal3DAnimation');
-    action = maya.cmds.getAttr(name_prefix+'.action');
-
-    # For global 3D animation, only run/pause actions support
-    if is_apply_to_3danimation and action > 1:
-        maya.cmds.warning( "3D animation only supports run/pause actions!" );
-        maya.cmds.setAttr('%s.action'%name_prefix, 0);
-            
+           
 class OSGAddTrigger:
+    def updateUI(self, name_prefix):
+        is_apply_to_3danimation = maya.cmds.getAttr(name_prefix+'.applyToGlobal3DAnimation');
+        action = maya.cmds.getAttr(name_prefix+'.action');
     
+        # For global 3D animation, only run/pause actions support
+        if is_apply_to_3danimation and action > 1:
+            maya.cmds.warning( "3D animation only supports run/pause actions!" );
+            maya.cmds.setAttr('%s.action'%name_prefix, 0);
+            
     def __init__(self):
         select_nodes = maya.cmds.ls(selection=True);
         if select_nodes:
             for one_select in select_nodes:
-                attrExist = maya.cmds.attributeQuery('triggers', node=one_select, exists=True);
-                if attrExist:
-                    maya.cmds.deleteAttr(one_select, at='triggers');
+                while True:
+                    attrExist = maya.cmds.attributeQuery('triggers', node=one_select, exists=True);
+                    if attrExist:
+                        maya.cmds.deleteAttr(one_select, at='triggers');
+                    else:
+                        break;
                 maya.cmds.addAttr(one_select, ln = "triggers", at="compound", nc = 6, m=True);
                 maya.cmds.addAttr(one_select, at = 'enum', keyable=True, en =TRIGGERS_DEF, ln='oneTrigger', p='triggers');
                 maya.cmds.addAttr(one_select, at = 'enum', keyable=True, en =ACTION_DEF, ln='action', p='triggers');
@@ -30,13 +34,5 @@ class OSGAddTrigger:
                 maya.cmds.addAttr(one_select, at = 'bool', ln='triggeredDefault', p='triggers', defaultValue=False);
                 maya.cmds.addAttr(one_select, at = 'bool', ln='applyToGlobal3DAnimation', p='triggers', defaultValue=False);
                 for i in range(100):
-                    maya.cmds.scriptJob(runOnce=False, attributeChange=[one_select+'.triggers[%d].action'%i, "updateUI('%s.triggers[%d]')"%(one_select, i)] );
-
-
-
-
-
-
-
-
-
+                    maya.cmds.scriptJob(runOnce=False, attributeChange=[one_select+'.triggers[%d].action'%i, functools.partial(self.updateUI, '%s.triggers[%d]'%(one_select, i))] );
+                    maya.cmds.scriptJob(runOnce=False, attributeChange=[one_select+'.triggers[%d].applyToGlobal3DAnimation'%i, functools.partial(self.updateUI, '%s.triggers[%d]'%(one_select, i))] );
